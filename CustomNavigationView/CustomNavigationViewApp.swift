857f92b66676e03afb41224a8acc4acb7ddf1772
Alex Bonney <bonney977@gmail.com>
2021-07-23 15:56:14 +0300

    Initial Commit

---
+++ CustomNavigationView/CustomNavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationViewApp.swift
+//  CustomNavigationView
+//
+//  Created by Alexander Bonney on 7/23/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
